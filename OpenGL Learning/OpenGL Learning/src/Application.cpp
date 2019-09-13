8a3266d5b5dedf63fd4a82542de806405b60291b
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-09-13 20:06:23 +0100

    Abstracted the shader string into its own .shader file and created a method to use that file

--- OpenGL Learning/OpenGL Learning/src/Application.cpp
+++ OpenGL Learning/OpenGL Learning/src/Application.cpp
@@ -1,6 +1,48 @@
 #include <glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos) //Checks to see if it can find #shader in line
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;			
+			else if (line.find("fragment") != std::string::npos)
+				type = ShaderType::FRAGMENT;
+			
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";	
+		}
+	}
+
+	return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -11,6 +53,8 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
 
 	int result;
 	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+	//If the result comes back false then an error will be thrown
 	if (result == GL_FALSE)
 	{
 		int length;
@@ -83,27 +127,15 @@ int main(void)
 	glEnableVertexAttribArray(0); //Enables the Vertex attributes array
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); //This says that the Vertices are 2 floats for each vertex
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 colour;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	colour = vec4(1.0, 0.0, 0.0, 1.0);"
-		"}\n";
-	
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+
+	std::cout << "VERTEX" << std::endl;
+	std::cout << source.VertexSource << std::endl;
+	std::cout << "FRAGMENT" << std::endl;
+	std::cout << source.FragmentSource << std::endl;
+
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 	
 	//glBindBuffer(GL_ARRAY_BUFFER, 0); //This unbinds the buffer
