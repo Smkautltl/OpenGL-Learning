fcb47fc31792ffb61ae2aaf5d1122460f523a1ab
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-09-17 12:28:25 +0100

    Implemented ImGui into project

--- OpenGL Learning/OpenGL Learning/src/Application.cpp
+++ OpenGL Learning/OpenGL Learning/src/Application.cpp
@@ -1,6 +1,3 @@
-#include <glew.h>
-#include <GLFW/glfw3.h>
-
 #include <iostream>
 #include <fstream>
 
@@ -12,8 +9,15 @@
 #include "shader.h"
 #include "Texture.h"
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <imgui/imgui.h>
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
+
+#include <glew.h>
+#include <GLFW/glfw3.h>
 
 
 int main(void)
@@ -27,6 +31,7 @@ int main(void)
         return -1;
 
 	//This sets the OpenGL version
+	const char* glsl_version = "#version 130";
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -83,15 +88,12 @@ int main(void)
     	//Sets up a Orthographic projection
 		glm::mat4 proj = glm::ortho(0.f, ViewWidth, 0.f, ViewHeight,-1.0f,1.0f);
 		glm::mat4 view = glm::translate(glm::mat4(1.f), glm::vec3(-100, 0 ,0));
-    	glm::mat4 model = glm::translate(glm::mat4(1.f), glm::vec3(200,200,0));
-
-		glm::mat4 mvp = proj * view * model;
     	
     	//Sets up the shader that will be used
 		Shader shader("res/shaders/Basic.shader");
     	shader.Bind();
     	shader.SetUniform4f("u_Colour", 0.7f, 0.3f, 0.5f, 1.0f);
-    	shader.SetUniformMat4f("u_MVP", mvp);
+    	
 
 		Texture texture("res/textures/marble.png");
 		texture.Bind();
@@ -104,7 +106,26 @@ int main(void)
 		ib.UnBind();
 
     	Renderer renderer;
+
+    	//Sets up imgui
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
 		
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsClassic();
+
+		// Setup Platform/Renderer bindings
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+    	
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+		glm::vec3 translation(200.f,200.f,0.f);
+    	
 		float r = 0.0f;
 		float increment = 0.05f;
 		/* Loop until the user closes the window */
@@ -112,7 +133,17 @@ int main(void)
 		{
 		    /* Render here */
 			renderer.Clear();
-			shader.Bind();		
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			glm::mat4 model = glm::translate(glm::mat4(1.f), translation);
+			glm::mat4 mvp = proj * view * model;
+				
+			shader.Bind();
+			shader.SetUniformMat4f("u_MVP", mvp);
+			
 			renderer.Draw(va, ib, shader);
 			
 			if (r > 1.0f)
@@ -120,6 +151,25 @@ int main(void)
 			else if (r < 0.0f)
 				increment = 0.01f;
 			r+=increment;
+
+			//imgui window setup
+			{
+				ImGui::ShowDemoWindow(&show_demo_window);
+
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Debug Tools");                    
+				ImGui::SliderFloat3("Translation", &translation.x, 0.f, ViewWidth);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+				
+				// Rendering
+				ImGui::Render();
+				ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			}
+			
 			
 		    /* Swap front and back buffers */
 		    GLCall(glfwSwapBuffers(window));
@@ -129,6 +179,11 @@ int main(void)
 			
 		}
 	}
+
+	//Shuts down imgui
+	ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 	
     glfwTerminate();
 	return 0;
